60cfc5d2272b4fa22b0c66b7623710dfb18e1191
gabrielnovais <gabrieel.novais@gmail.com>
2022-11-23 20:53:16 -0300

    adjusting ec2...

--- infrastructure/aws/ec2.tf
+++ infrastructure/aws/ec2.tf
@@ -2,11 +2,19 @@ data "template_file" "init" {
   template = "./CI/scripts/startup.sh"
 }
 
+data "aws_key_pair" "example" {
+  key_name = "terraform"
+  filter {
+    name   = "tag:Component"
+    values = ["web"]
+  }
+}
+
 # https://cloud-images.ubuntu.com/locator/ec2/
 resource "aws_instance" "airflow-iac" {
   ami           = "ami-0b4577d77dac11b84"
   instance_type = "t3.medium"
-  key_name = "airflow-iac"
+  key_name = data.aws_key_pair.example.key_name
 
   vpc_security_group_ids = [aws_security_group.allow-all-traffic-vpn-ingress.id, aws_security_group.allow-all-traffic-vpn-egress.id] 
 
