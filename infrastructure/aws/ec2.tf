d564ec15279d3bb015b96def0bcfa94e361c1792
gabrielnovais <gabrieel.novais@gmail.com>
2022-11-23 21:57:55 -0300

    adjusting ec2...

--- infrastructure/aws/ec2.tf
+++ infrastructure/aws/ec2.tf
@@ -4,7 +4,7 @@ data "template_file" "init" {
 
 resource "aws_key_pair" "mk" {
   key_name = "mk"
-  public_key = "./id_rsa.pub"
+  public_key = "./conectaEC2.pem"
 }
 
 # https://cloud-images.ubuntu.com/locator/ec2/
