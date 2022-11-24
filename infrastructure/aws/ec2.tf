42e011f38e793e010d1e43c66cce0503868d7358
gabrielnovais <gabrieel.novais@gmail.com>
2022-11-24 11:22:26 -0300

    More tests 9

--- infrastructure/aws/ec2.tf
+++ infrastructure/aws/ec2.tf
@@ -2,10 +2,25 @@ data "template_file" "init" {
   template = "./CI/scripts/startup.sh"
 }
 
-resource "aws_instance" "airflow-iac" {
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "TFkey" {
+  key_name   = "newkey" 
+  public_key = tls_private_key.pk.public_key_openssh
+}
+
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.TFkey.key_name}.pem"
+  content = tls_private_key.pk.private_key_pem
+}
+
+resource "aws_instance" "web" {
   ami           = "ami-0b4577d77dac11b84"
   instance_type = "t3.medium"
-  key_name      = "public-key"
+  key_name      = aws_key_pair.TFkey.key_name
 
   vpc_security_group_ids = [aws_security_group.allow-all-traffic-vpn-ingress.id, aws_security_group.allow-all-traffic-vpn-egress.id]
 
@@ -14,7 +29,7 @@ resource "aws_instance" "airflow-iac" {
   user_data = data.template_file.init.rendered
 
   tags = {
-    Name = "airflow-iac"
+    Name = "myec2"
   }
 }
 
