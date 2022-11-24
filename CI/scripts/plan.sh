753c8e73f75601a3d99317fc7d4b501107298ede
gabrielnovais <gabrieel.novais@gmail.com>
2022-11-24 10:15:45 -0300

    More tests 2

--- CI/scripts/plan.sh
+++ CI/scripts/plan.sh
@@ -7,7 +7,7 @@ set -e
 
 cd infrastructure/aws/
 terraform init
-terraform state replace-provider -auto-approve registry.terraform.io/-/vault registry.terraform.io/hashicorp/vault
+# terraform state replace-provider -auto-approve registry.terraform.io/-/vault registry.terraform.io/hashicorp/vault
 terraform workspace select $1 || terraform workspace new $1
 terraform plan
 cd ../../
